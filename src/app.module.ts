38a9c253ae1ec3c339cb3fa1f24fb644c1e5b18d
Sarmad Naroo <sarmadnarooali@gmail.com>
2025-07-03 17:34:41 +0500

    setup docker for nestjs and redis

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { QueueModule } from './queue/queue.module';
+import { ConfigModule } from '@nestjs/config';
+import { TaskModule } from './task/task.module';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot({ isGlobal: true }), QueueModule, TaskModule],
   controllers: [AppController],
   providers: [AppService],
 })
